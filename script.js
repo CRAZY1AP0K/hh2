045dd83c0439e0dc3176300d8db1e0e87401268a
Макар <122713760+CRAZY1AP0K@users.noreply.github.com>
2024-03-19 01:49:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showText() {
+    document.getElementById('hidden-text').style.display = 'block';
+    var img = document.createElement("img");
+    img.src = "https://media.tenor.com/sVNO62-MYV0AAAAM/zxc-cat.gif";
+    document.body.appendChild(img);
+    
+}
